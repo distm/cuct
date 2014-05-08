fcb0d239c57bdb54009d6b862a4790607ea55b92
Apm Server <apm@server>
2014-05-08 17:30:52 +0700

    initial commit

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+    
+    var $_data;
+    
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+}
